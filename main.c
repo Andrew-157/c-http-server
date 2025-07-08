2d00e588ffcf60b632153d8dc927bc2ed40aa30b
Andrew-157 <subotinandrey5@gmail.com>
2025-07-08 19:16:03 +0200

    Added more test data to check that the way to divide the request message is valid so far

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 
 int parse_http_request_message(char *);
 
+
 int main() {
     char *test_data[10] = {
         "GET /index.html HTTP/1.1\r\nHost: example.com\r\nConnection: close\r\n\r\n", // valid
@@ -10,10 +11,14 @@ int main() {
         "POST /users HTTP/2.0\r\n\r\nSome Body, idk", // valid
         "", // invalid
         "PATCH /edit HTTP/1.1\r\n\r\nadjust=data", // valid
-        "GET /items/1 HTTP/1.1\r\nHost: site.com\r\n\r\n" // valid
+        "GET /items/1 HTTP/1.1\r\nHost: site.com\r\n\r\n", // valid
+        "POST /users HTTP/1.1\r\nHost: mywebsite.auth.com\r\nConnection: close\r\nContent-Type: application/json\r\n\r\n{\"user\":\n  {  \n\"password\": 1234\n}\n}"
     };
-    for (int i = 0; i < 6; ++i)
+    for (int i = 0; i < 7; ++i) {
+        printf("------------------------------------\n");
         parse_http_request_message(test_data[i]);
+        printf("------------------------------------\n");
+    }
 }
 
 
@@ -29,6 +34,8 @@ int parse_http_request_message(char *message) {
 
     // NOTE: Get indices of first CRLF and CRLF that is followed by one more CRLF, this way
     // the request message can be divided
+    printf("%s\n", message);
+
     int first_crlf_index = -1;
     int empty_line_crlf_index = -1;
     int message_len = strlen(message);
@@ -58,8 +65,30 @@ int parse_http_request_message(char *message) {
         printf("ERROR: message doesn't contain double CRLF\n");
         return 3;
     }
+    //printf("Index of first CRLF: %d\n", first_crlf_index);
+    //printf("Index of empty line CRLF: %d\n", empty_line_crlf_index);
 
-    printf("Index of first CRLF: %d\n", first_crlf_index);
-    printf("Index of empty line CRLF: %d\n", empty_line_crlf_index);
+    // Now with found info print request line, headers and body
+    printf("DEBUG: Printing request line:\n");
+    for (int i = 0; i < first_crlf_index; ++i)
+        putchar(message[i]);
+    putchar('\n');
+
+    if (first_crlf_index != empty_line_crlf_index) {
+        printf("DEBUG: Printing headers:\n");
+        for (int i = (first_crlf_index+2); i < empty_line_crlf_index; ++i)
+            putchar(message[i]);
+        putchar('\n');
+    } else
+        printf("DEBUG: Headers are not present\n");
+
+    if (message[empty_line_crlf_index+4] != '\0') {
+        printf("DEBUG: Printing body:\n");
+        for (int i = (empty_line_crlf_index+4); message[i] != '\0'; ++i)
+            putchar(message[i]);
+        putchar('\n');
+    } else
+        printf("DEBUG: Body is not present\n");
+
     return 0;
 }
