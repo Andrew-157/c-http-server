728872eaa3d52d549323552fad74cdf70a4662fe
Andrew-157 <subotinandrey5@gmail.com>
2025-07-10 16:51:56 +0200

    commented the previous implementation so far

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 
 void parse_http_request_message(char *);
 
+char METHOD[8];
 
 int main() {
     char *test_data[10] = {
@@ -15,42 +16,42 @@ int main() {
         "", // invalid
         "PATCH /edit HTTP/1.1\r\n\r\nadjust=data", // valid
         "GET /items/1 HTTP/1.1\r\nHost: site.com\r\n\r\n", // valid
-        "POST /users HTTP/1.1\r\nHost: mywebsite.auth.com\r\nConnection: close\r\nContent-Type: application/json\r\n\r\n{\"user\":\n  {  \n\"password\": 1234\n}\n}"
+        "POST /users HTTP/1.1\r\nHost: mywebsite.auth.com\r\nConnection: close\r\nContent-Type: application/json\r\n\r\n{\"user\":\n  {  \n\"password\": 1234\n}\n}",
+        " HEAD /data HTTP/1.1\r\n\r\n",
     };
 }
 
-
-void parse_http_request_message(char *message) {
-    int first_crlf_index = -1;
-    int empty_line_crlf_index = -1;
-    int message_len = strlen(message);
-
-    for (int i = 0; message[i] != '\0'; ++i) {
-        if (message[i] == '\r') {
-            if ((i+1) > message_len || message[i+1] != '\n') {
-                printf("ERROR: '\\r' is not followed by '\\n'\n");
-                return;
-            } else if (message[i+1] == '\n') {
-                if (first_crlf_index < 0)
-                    first_crlf_index = i;
-                if ((i+3) <= message_len && message[i+2] == '\r' && message[i+3] == '\n') {
-                    empty_line_crlf_index = i;
-                    break;
-                } else
-                  ++i;
-            }
-        }
-    }
-
-    if (first_crlf_index < 0) {
-        printf("ERROR: CRLF is not found in message\n");
-        return;
-    }
-    if (empty_line_crlf_index < 0) {
-        printf("ERROR: message doesn't contain double CRLF\n");
-        return;
-    }
-
-    return;
-}
-
+// void parse_http_request_message(char *message) {
+//     int first_crlf_index = -1;
+//     int empty_line_crlf_index = -1;
+//     int message_len = strlen(message);
+// 
+//     for (int i = 0; message[i] != '\0'; ++i) {
+//         if (message[i] == '\r') {
+//             if ((i+1) > message_len || message[i+1] != '\n') {
+//                 printf("ERROR: '\\r' is not followed by '\\n'\n");
+//                 return;
+//             } else if (message[i+1] == '\n') {
+//                 if (first_crlf_index < 0)
+//                     first_crlf_index = i;
+//                 if ((i+3) <= message_len && message[i+2] == '\r' && message[i+3] == '\n') {
+//                     empty_line_crlf_index = i;
+//                     break;
+//                 } else
+//                   ++i;
+//             }
+//         }
+//     }
+// 
+//     if (first_crlf_index < 0) {
+//         printf("ERROR: CRLF is not found in message\n");
+//         return;
+//     }
+//     if (empty_line_crlf_index < 0) {
+//         printf("ERROR: message doesn't contain double CRLF\n");
+//         return;
+//     }
+// 
+//     return;
+// }
+// 
