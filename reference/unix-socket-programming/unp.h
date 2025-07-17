c6d6b223bc2634352e9e17b1ef2bc31e85a12165
Andrew-157 <subotinandrey5@gmail.com>
2025-07-17 21:00:50 +0200

    continuing writing unp.h

--- reference/unix-socket-programming/unp.h
+++ reference/unix-socket-programming/unp.h
@@ -34,3 +34,47 @@
 #include <poll.h>  /* for convenience */
 #endif
 
+#ifdef HAVE_SYS_EVENT_H
+#include <sys/event.h>  /* for kqueue */
+#endif
+
+#ifdef HAVE_STRINGS_H
+#include <strings.h>  /* for convenience */
+#endif
+
+/* Three headers are normally needed for socket/file ioctl's:
+ * <sys/ioctl.h>, <sys/filio.h>, and <sys/sockio.h>.
+ * */
+#ifdef HAVE_SYS_IOCTL_H
+#include <sys/ioctl.h>
+#endif
+
+#ifdef HAVE_SYS_FILIO_H
+#include <sys/filio.h>
+#endif
+
+#ifdef HAVE_SYS_SOCKIO_H
+#include <sys/sockio.h>
+#endif
+
+#ifdef HAVE_PTHREAD_H
+#include <pthread.h>
+#endif
+
+#ifdef HAVE_NET_IF_DL_H
+#include <net/if_dl.h>
+#endif
+
+#ifdef HAVE_NETINET_SCTP_H
+#include <netinet/sctp.h>
+#endif
+
+/* OSF/1 actually disables recv() and send() in <sys/socket.h> */
+#ifdef __osf__
+#undef recv
+#under send
+#define recv(a,b,c,d) recvfrom(a,b,c,d,0,0)
+#define send(a,b,c,d) sendto(a,b,c,d,0,0)
+#endif
+
+
