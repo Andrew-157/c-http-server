88c1fac2dc2b584e7a3b3b545639f57caa97d2b5
Andrew-157 <subotinandrey5@gmail.com>
2025-07-25 12:18:38 +0200

    implementing server again using getaddrinfo #1

--- src/server.c
+++ src/server.c
@@ -1,103 +1,86 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
 #include <string.h>
-#include <unistd.h> // contains read, write, etc.
-#include <arpa/inet.h> // contains stuff for socket manipulations
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
 
-#define BUFFER_SIZE 1024
-#define PORT 8080
+
+#define PORT 80
+#define BACKLOG 10 // Right now I can do absolutely nothing with that, but just so I know about that param
+
 
 int main() {
-    int server_fd, client_fd;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
-    char buffer[BUFFER_SIZE];
 
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == -1) {
-        fprintf(stderr, "Server socket creation failed\n");
-        exit(EXIT_FAILURE);
+    int sockfd, new_fd;
+    socklen_t sin_size;
+    struct sockaddr_storage their_addr;
+    struct addrinfo hints, *servinfo, *p;
+    char s[INET_ADDRSTRLEN];
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET; // Could be AF_UNSPEC, to be IP version agnostic
+    hints.ai_socktype = SOCK_STREAM; // TCP
+    hints.ai_flags = AI_PASSIVE; // Use my IP
+
+    if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gaierror(rv));
+        exit(errno);
     }
 
-    printf("Created server socket successfully\n");
+    // I honestly don't understand why we need to do it to create server structure if we already
+    // know its parameters, but whatever
+    for (p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+            perror("server: socket");
+            continue;
+        }
 
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)) == -1) {
-        fprintf(stderr, "Setting of socket options failed\n");
-        exit(EXIT_FAILURE);
+        int yes = 1;
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1) { // Allows to avoid "Address already in use" error
+            perror("setsockopt");
+            exit(errno);
+        }
+
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("server: bind");
+            continue;
+        }
+
+        break;
     }
 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = htonl(INADDR_ANY);
-    address.sin_port = htons(PORT);
+    freeaddrinfo(servinfo); // all done with this structure
 
-    if (bind(server_fd, (struct sockaddr *)&address, addrlen) == -1) {
-        fprintf(stderr, "Server socket bind failed\n");
-        exit(EXIT_FAILURE);
+    if (p == NULL) {
+        fprintf(stderr, "server: failed to bind\n");
+        exit(errno);
     }
 
-    if (listen(server_fd, 3) == -1) {
-        fprintf(stderr, "Listen failed\n");
-        exit(EXIT_FAILURE);
-    } else
-        printf("Server is listening on %d\n", PORT);
-
-    client_fd = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
-    if (client_fd == -1) {
-        fprintf(stderr, "Failed to accept client connection\n");
-        exit(EXIT_FAILURE);
-    }
-    else
-        printf("Accepted client connection\n");
-
-    struct sockaddr_in client_address;
-    int client_address_len = sizeof(client_address);
-    getpeername(client_fd, (struct sockaddr *)&client_address, (socklen_t *)&client_address_len);
-
-    printf("Client data: %d, %s, %d\n", client_address.sin_family, inet_ntoa(client_address.sin_addr), client_address.sin_port);
-
-    // TODO: handle saving data to buffer by adjust index or something like that, idk
-    //ssize_t valread = read(client_fd, buffer, BUFFER_SIZE); // Waaaait, what if BUFFER SIZE is not enough for what client sent me?
-    //printf("Read from client socket %ld bytes of data: %s\n", valread, buffer);
-    //memset(buffer, 0, strlen(buffer));
-
-    // NOTE: This loop doesn't work
-    ssize_t valread;
-    // Most likely the problem is that read TRIES to read whole BUFFER_SIZE
-    while ((valread = read(client_fd, buffer, BUFFER_SIZE)) > 0) {
-        printf("Received from client %ld bytes with data: %s\n", valread, buffer);
-        memset(buffer, 0, sizeof(buffer));
+    if (listen(sockfd, BACKLOG) == -1) {
+        perror("listen");
+        exit(errno);
     }
 
-    // TODO: handle writing to socket until all the data was written
-    char http_response[] = "HTTP/1.1 200 OK\r\n\\"
-                           "Content-Type: text/html\r\n"
-                           "Content-Length: 97\r\n"
-                           "Connection: close\r\n"
-                           "\r\n"
-                           "<!DOCTYPE html>"
-                           "<html>"
-                           "<head><title>Minimal HTML</title></head>"
-                           "<body>Hello, world!</body>"
-                           "</html>";
-    ssize_t valwritten = write(client_fd, http_response, strlen(http_response));
-    printf("Written bytes %ld vs expected written bytes %ld\n", valwritten, strlen(http_response));
+    printf("server: waiting for connections...\n");
 
-    // Try to write to client socket in chunks - WORKS!
-    //char first_part[] = "HTTP/1.1 200";
-    //char second_part[] = " OK\r\n\r\n";
-    //write(client_fd, first_part, strlen(first_part));
-    //write(client_fd, second_part, strlen(second_part));
+    // There are 10 BACKLOG connections, but I am accepting only one for now,
+    // moreover, it means that after accepting one client socket, I can close server socket
+    sin_size = sizeof their_addr;
+    new_fd = accept(sockfd, (struct sockaddr *)their_addr, &sin_size);
+    if (new_fd == -1) {
+        perror("accept");
+        continue;
+    }
 
-    // For some reason before client socket can be closed, to make client receive response properly, I either need to:
-    // - sleep
+    inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr), s, sizeof(s));
+    printf("server: got connection from %s\n", s);
 
-    sleep(1);
 
-    //read(client_fd, buffer, BUFFER_SIZE);
-    //printf("Read from client socket again: %s\n", buffer);
-    //memset(buffer, 0, strlen(buffer));
 
-    close(server_fd);
-    exit(0);
 }
