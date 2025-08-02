a6ca50dabd683872927521067086e413c8701e58
Andrew-157 <subotinandrey5@gmail.com>
2025-08-02 16:14:25 +0200

    Server in multiple functions, but better this time

--- src/server.c
+++ src/server.c
@@ -9,138 +9,82 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 
-#define PORT               "8080" // server port
-#define BACKLOG            10     // number of pending connections
-#define RECV_MSG_BUFFER    1024   // buffer size for recv method
+#define PORT            "8080" // port on which to listen for incoming connections
+#define BACKLOG         10     // maximum number of pending connections
+#define RECV_MSG_BUFFER 1024   // recv buffer
+
+int create_server_socket(char *port);
+char *read_template(char *template_path);
 
 int main() {
-    int server_sockfd, client_sockfd;
-    struct addrinfo hints, *servinfo, *p;
-    socklen_t sin_size;
-    struct sockaddr_in client_address;
-    char printable_ip[INET_ADDRSTRLEN];
+    int server_sockfd;
+    server_sockfd = create_server_socket(PORT);
 
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family   = AF_INET;   // to make it IP version agnostic, set AF_UNSPEC
-    hints.ai_socktype = SOCK_STREAM; // TCP socket
-    hints.ai_flags    = AI_PASSIVE;
-
-    int result;
-    if ((result = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(result));
-        exit(errno);
-    }
-
-    for (p = servinfo; p != NULL; p = p->ai_next) {
-        if ((server_sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
-            perror("server: socket creation");
-            continue;
-        }
-
-        // SO_REUSEADDR allows to avoid "Address already in use" error
-        int yes = 1;
-        if (setsockopt(server_sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1) {
-            close(server_sockfd);
-            perror("server: setsockopt");
-            continue;
-        }
-
-        /* If after 5 seconds no client connects, `accept` fails on "Resource temporarily unavailable"
-        struct timeval timeout;
-        timeout.tv_sec = 5;
-        timeout.tv_usec = 0;
-        if (setsockopt(server_sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout)) == -1) {
-            close(server_sockfd);
-            perror("server: setsockopt");
-            continue;
-        }
-        */
-
-        if (bind(server_sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-            close(server_sockfd);
-            perror("server: bind"); // Exemplary error: "server: bind: Permission denied" - when trying to use port 80, for example
-            continue;
-        }
-
-        break;
-    }
-
-    freeaddrinfo(servinfo); // all done with this structure
-
-    if (p == NULL) {
-        fprintf(stderr, "failed to establish server socket\n");
+    if (server_sockfd == -1) {
+        fprintf(stderr, "Failed to create server socket: %s\n", strerror(errno));
         exit(errno);
     }
 
     if (listen(server_sockfd, BACKLOG) == -1) {
-        perror("server: listen");
+        fprintf(stderr, "listen: %s\n", strerror(errno));
         exit(errno);
     }
 
     printf("accepting client connections...\n");
 
+    int client_sockfd;
+    socklen_t sin_size;
+    struct sockaddr_in client_address;
+    char printable_ip[INET_ADDRSTRLEN];
+
     sin_size = sizeof client_address;
-    client_sockfd = accept(server_sockfd, (struct sockaddr *)&client_address, &sin_size);
-    if (client_sockfd == -1) {
-        perror("server: accept");
+    if ((client_sockfd = accept(server_sockfd, (struct sockaddr *)&client_address, &sin_size)) == -1) {
+        close(server_sockfd);
+        fprintf(stderr, "accept: %s\n", strerror(errno));
         exit(errno);
     }
 
-    /* If after 5 seconds, client doesn't send data, `recv` will fail with "recv: Resource temporarily unavailable"
+    // timeout on `recv`
     struct timeval timeout;
     timeout.tv_sec = 5;
     timeout.tv_usec = 0;
-    setsockopt(client_sockfd, SOL_SOCKET, SOL_RCVTIMEO, &timeout, sizeof(timeout));
-    */
+    if (setsockopt(client_sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout)) == -1) {
+        close(server_sockfd);
+        close(client_sockfd);
+        fprintf(stderr, "setsockopt: %s\n", strerror(errno));
+        exit(errno);
+    }
 
     inet_ntop(client_address.sin_family, &client_address.sin_addr, printable_ip, sizeof(printable_ip));
-    printf("server: accepted connection from %s:%d\n", printable_ip, ntohs(client_address.sin_port));
+    printf("accepted client connection from IP(%s):PORT(%d)\n", printable_ip, ntohs(client_address.sin_port));
 
-    printf("Reading data from client\n");
+    printf("Reading from client socket\n");
 
     char recv_msg[RECV_MSG_BUFFER];
     int bytes_received;
     bytes_received = recv(client_sockfd, recv_msg, RECV_MSG_BUFFER, 0);
-    // bytes_received == 0 - as I understand it happens when client closes the connection
-    // bytes_received == -1 - error
+    // bytes_received == 0  - client closed connection ... I guess
+    // bytes_received == -1 - error occurred
     if (bytes_received == 0) {
-        printf("looks like client closed the connection, ending communication\n");
+        printf("client closed connection, ending communication\n");
         close(client_sockfd);
         close(server_sockfd);
         exit(0);
     } else if (bytes_received == -1) {
-        perror("server: recv");
+        fprintf(stderr, "recv: %s\n", strerror(errno));
         close(client_sockfd);
         close(server_sockfd);
         exit(errno);
     }
-
     recv_msg[bytes_received] = '\0';
-    printf("Received message from client:\n\n%s\n", recv_msg);
+    printf("Message from client:\n%s\n", recv_msg);
 
-    printf("Sending an HTTP response to client with body with HTML Content-Type\n");
+    printf("Sending an HTTP response to client with HTML body\n");
 
-    const char *http_response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: 232\r\n\r\n";
-
-    // read template and put it into string
-    FILE *file;
-    file = fopen("./templates/index.html", "r");
-    fseek(file, 0, SEEK_END); // Moves file pointer to the end of the file
-    int length = ftell(file);
-    fseek(file, 0, SEEK_SET); // Moves file pointer back to the beginning of the file
-
-    // Do I need to use malloc or can I just do char html[length]?
-    char *html = malloc(sizeof(char) * (length + 1)); // length + 1 because of '\0'
-
-    int c, i;
-    i = 0;
-    while ((c = getc(file)) != EOF) // it could be fgetc instead of getc
-        html[i++] = c;
-    html[i] = '\0';
-    fclose(file);
-
+    char *http_response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: 232\r\n\r\n";
+    char *html = read_template("./templates/index.html");
     send(client_sockfd, http_response, strlen(http_response), 0);
-    send(client_sockfd, html, length, 0);
+    send(client_sockfd, html, strlen(html), 0);
     free(html);
 
     printf("Ending communication\n");
@@ -148,4 +92,67 @@ int main() {
     close(server_sockfd);
 
     exit(0);
+}
+
+int create_server_socket(char *port) {
+    int sockfd;
+    struct addrinfo hints, *servinfo, *p;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family   = AF_INET;     // IPv4, to be IP-version-agnostic, set it to AF_UNSPEC
+    hints.ai_socktype = SOCK_STREAM; // TCP
+    hints.ai_flags    = AI_PASSIVE;
+
+    int result;
+    if ((result = getaddrinfo(NULL, port, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(result));
+        exit(errno);
+    }
+
+    for (p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+            fprintf(stderr, "socket: %s\n", strerror(errno));
+            continue;
+        }
+
+        int yes = 1;
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1) {
+            sockfd = -1;
+            close(sockfd);
+            fprintf(stderr, "setsockopt: %s\n", strerror(errno));
+            continue;
+        }
+
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            sockfd = -1;
+            close(sockfd);
+            fprintf(stderr, "bind: %s\n", strerror(errno));
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL)
+        fprintf(stderr, "server: failed to bind\n");
+
+    freeaddrinfo(servinfo); // all done with this structure
+    return sockfd;
+}
+
+char *read_template(char *template_path) {
+    FILE *file;
+    file = fopen(template_path, "r");
+    fseek(file, 0, SEEK_END); // Moves file pointer to the end of the file
+    int length = ftell(file);
+    fseek(file, 0, SEEK_SET); // Moves file pointer back to the beginning of the file
+
+    char *html = malloc(sizeof(char) * (length + 1)); // length + 1 because of '\0'
+    int c, i;
+    i = 0;
+    while ((c = getc(file)) != EOF) // it could be `fgetc` instead of `getc`
+        html[i++] = c;
+    html[i] = '\0';
+    fclose(file);
+    return html;
 }
