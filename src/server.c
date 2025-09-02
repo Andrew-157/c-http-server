4dc61b714f7259bf3862c4f344bbc3ecbdfbeca6
Andrew-157 <subotinandrey5@gmail.com>
2025-09-02 21:56:32 +0200

    read headers one by one

--- src/server.c
+++ src/server.c
@@ -215,6 +215,7 @@ char * accept_rqst(int client_sockfd, int recv_msg_buffer_size) {
 
     log_message(INFO, "Bytes received: %d\n", chunk_bytes_received);
 
+    // utils for reading request line
     char rqst_line[100]; // i don't have a slightest idea what would be the correct size
     int rqst_line_read = 0;
     char method[8]; // is OPTIONS the longest method?
@@ -224,12 +225,25 @@ char * accept_rqst(int client_sockfd, int recv_msg_buffer_size) {
     char protocol[9]; // yeah, im sure this time
     int protocol_read = 0;
 
+    // utils for reading headers
+    char curr_header[100]; // variable to store one header at a time
+
+    // int curr;
+
     int j = 0; // index for keeping track of element in method, url, protocol
     for (int i = 0; i < chunk_bytes_received; i++) {
+        //curr = recv_msg[i];
+        //if (curr == '\n')
+        //    log_message(DEBUG, "Current character is newline\n");
+        //else if (curr == '\r')
+        //    log_message(DEBUG, "Current character is carriage return\n");
+        //else
+        //    log_message(DEBUG, "Current character is: %c\n", curr);
         if ((recv_msg[i] == '\r') && ((i+1) < chunk_bytes_received) && (recv_msg[i+1] == '\n')) {
             log_message(DEBUG, "Encountered CRLF\n");
             if (!rqst_line_read) {
                 protocol[j] = '\0';
+                j = 0;
                 protocol_read = 1;
                 rqst_line[i] = '\0';
                 rqst_line_read = 1;
@@ -238,19 +252,30 @@ char * accept_rqst(int client_sockfd, int recv_msg_buffer_size) {
                 log_message(DEBUG, "Request message method is: %s\n", method);
                 log_message(DEBUG, "Request message url is: %s\n", url);
                 log_message(DEBUG, "Request message protocol is: %s\n", protocol);
-            }
+             } else {
+                curr_header[j] = '\0';
+                j = 0;
+                log_message(DEBUG, "Read a header: %s\n", curr_header);
+             }
              if ((i+3) < chunk_bytes_received && recv_msg[i+2] == '\r' && recv_msg[i+3] == '\n') {
                  log_message(DEBUG, "Encountered double CRLF\n");
                  if ((i+4) < chunk_bytes_received) {
                      log_message(DEBUG, "There is more to read after double CRLF, continuing\n");
+                     i += 3;
                      continue;
                  }
                  log_message(DEBUG, "HTTP request message has been completely read\n");
                  break;
-             }
+             } else
+                 i++; // skip next character which is '\n'
+             continue;
         }
+
         if (!rqst_line_read)
             rqst_line[i] = recv_msg[i];
+        else
+            curr_header[j++] = recv_msg[i];
+
         if (recv_msg[i] == ' ') {
             if (!method_read) {
                 method[j] = '\0';
